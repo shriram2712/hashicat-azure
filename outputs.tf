c0cf8ea9fcc5407a993c5975276109340d4d9f39
Shriram Rajaraman <shriram.rajaraman@hashicorp.com>
2024-04-25 13:29:11 +0530

    Added network components only

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,15 @@
 # output "catapp_ip" {
 #   value = "http://${azurerm_public_ip.catapp-pip.ip_address}"
 # }
+
+output "azurerm_virtual_network" {
+  value = azurerm_virtual_network.vnet.name
+}
+
+output "azurerm_virtual_subnet" {
+  value = azurerm_subnet.subnet.name
+}
+
+output "azurerm_resource_group" {
+  value = azurerm_resource_group.myresourcegroup.name
+}
